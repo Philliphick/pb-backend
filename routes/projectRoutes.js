9a4073c0d5fa60ff064831d6b44ca8d0b148041a
Philliphick <81019680+Philliphick@users.noreply.github.com>
2024-02-07 13:55:12 +0000

    implemented controller and schema for creating a user

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const { getProjects } = require("../controllers/getController");
 const { getAllProjects } = require("../controllers/getAllController");
+const { makeUser } = require("../controllers/makeUserController");
+
 
 
 router.get("/:nameParam", getProjects);
 router.get("/", getAllProjects);
+router.post("/newUser", makeUser);
 
 
 module.exports = router;
