dd0942b4e85f30ce208e1a4b571a4a36384e7c9d
d8a0 <151666577+d8a0@users.noreply.github.com>
2024-02-13 14:39:05 +0000

    minor changes

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use(cors());
 app.use(router);
 
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+app.listen(5001, () => console.log("Server started on port 5000"));
 
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,18 +1,20 @@
 const express = require("express")
 const router = express.Router();
 
-
 const { getProjects } = require("../controllers/getController");
 const { getAllProjects } = require("../controllers/getAllController");
 
 const { makeUser } = require("../controllers/makeUserController");
-const { deleteProject } = require("../controllers/deleteController");
+const { deletePost } = require("../controllers/deletePostController");
 
+const { makePost } = require("../controllers/makePostController")
 
 router.get("/:nameParam", getProjects);
 router.get("/", getAllProjects);
 router.post("/newUser", makeUser);
 
-router.delete("/:id", deleteProject);
+router.post("/makePost", makePost);
+
+router.delete("/delete/:_id", deletePost);
 
 module.exports = router;
